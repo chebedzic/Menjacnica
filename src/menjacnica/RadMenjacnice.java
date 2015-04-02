45ea0edb0fc132cdc1942925e3a795dcf73e546e
chebedzic <aleksachebedzic@gmail.com>
2015-04-02 15:41:03 +0200

    Napravljena klasa koja implementira interfejs. Metode su nefunkcionalne.

---
+++ src/menjacnica/RadMenjacnice.java
@@ -0,0 +1,25 @@
+package menjacnica;
+
+import java.util.GregorianCalendar;
+
+import valute.Valuta;
+
+public class RadMenjacnice implements MenjacnicaInterfejs {
+
+	public void dodavanjeKursa(GregorianCalendar gc, float srednjiKurs,
+			float kupovniKurs, float prodajniKurs, Valuta valuta) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void brisanjeKursa(GregorianCalendar gc) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void pronalazenjeKursa(GregorianCalendar gc) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
