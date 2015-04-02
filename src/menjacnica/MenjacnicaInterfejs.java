3e0e5897f74578de8744f6615abba71fda66b9a3
chebedzic <aleksachebedzic@gmail.com>
2015-04-02 16:11:57 +0200

    zavrsena implementacija interfejsa

--- src/menjacnica/MenjacnicaInterfejs.java
+++ src/menjacnica/MenjacnicaInterfejs.java
@@ -3,9 +3,10 @@ package menjacnica;
 import java.util.GregorianCalendar;
 
 import valute.Valuta;
+import valute.Vrednost;
 
 public interface MenjacnicaInterfejs {
 public void dodavanjeKursa(GregorianCalendar gc, float srednjiKurs, float kupovniKurs, float prodajniKurs, Valuta valuta);
 public void brisanjeKursa(GregorianCalendar gc);
-public void pronalazenjeKursa(GregorianCalendar gc);
+public Vrednost pronalazenjeKursa(GregorianCalendar gc);
 }
--- src/menjacnica/RadMenjacnice.java
+++ src/menjacnica/RadMenjacnice.java
@@ -3,22 +3,41 @@ package menjacnica;
 import java.util.GregorianCalendar;
 
 import valute.Valuta;
+import valute.Vrednost;
 
 public class RadMenjacnice implements MenjacnicaInterfejs {
+	Vrednost[] kursevi = new Vrednost[365];
+	int br = 0;
 
 	public void dodavanjeKursa(GregorianCalendar gc, float srednjiKurs,
 			float kupovniKurs, float prodajniKurs, Valuta valuta) {
-		// TODO Auto-generated method stub
-
+		Vrednost vr = new Vrednost();
+		vr.setDatum(gc);
+		vr.setKupvniKurs(kupovniKurs);
+		vr.setNazivValute(valuta);
+		vr.setProdajniKurs(prodajniKurs);
+		vr.setSrednjiKurs(srednjiKurs);
+		kursevi[br] = vr;
 	}
 
 	public void brisanjeKursa(GregorianCalendar gc) {
-		// TODO Auto-generated method stub
+		for (int i = 0; i < kursevi.length; i++) {
+			if (kursevi[i].getDatum().equals(gc)) {
+				kursevi[i] = null;
+				return;
+			}
+		}
 
 	}
 
-	public void pronalazenjeKursa(GregorianCalendar gc) {
-		// TODO Auto-generated method stub
+	public Vrednost pronalazenjeKursa(GregorianCalendar gc) {
+		for (int i = 0; i < kursevi.length; i++) {
+			if (kursevi[i].getDatum().equals(gc)) {
+				return kursevi[i];
+			}
+
+		}
+		throw new RuntimeException("Nema trazenog datuma");
 
 	}
 
