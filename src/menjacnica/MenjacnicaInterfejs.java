b90bc838369fe0004d224a6fad8f72f3c6be178c
chebedzic <aleksachebedzic@gmail.com>
2015-04-02 15:35:58 +0200

    prepravljene neke greske iz interfejsa MenjacnicaInterfejs

--- src/menjacnica/MenjacnicaInterfejs.java
+++ src/menjacnica/MenjacnicaInterfejs.java
@@ -2,8 +2,10 @@ package menjacnica;
 
 import java.util.GregorianCalendar;
 
+import valute.Valuta;
+
 public interface MenjacnicaInterfejs {
-public void dodavanjeKursa(GregorianCalendar gc);
+public void dodavanjeKursa(GregorianCalendar gc, float srednjiKurs, float kupovniKurs, float prodajniKurs, Valuta valuta);
 public void brisanjeKursa(GregorianCalendar gc);
 public void pronalazenjeKursa(GregorianCalendar gc);
 }
